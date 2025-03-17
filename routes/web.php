2f079570eb477020bfb58a3c45b29f120294a532
yuri <100086541+leticiam2004@users.noreply.github.com>
2025-03-17 16:17:01 -0300

    teste

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\AlunoController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/aluno', [AlunoController::class,'index'])->name('aluno.index');
